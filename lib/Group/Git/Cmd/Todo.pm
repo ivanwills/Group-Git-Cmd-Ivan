dc6e03ff242e4cc06e450476b80df3f46c2e76f2
Ivan Wills <ivan.wills@gmail.com>
2015-02-04 07:00:55 +1100

    New TODO summarizer

---
+++ lib/Group/Git/Cmd/Todo.pm
@@ -0,0 +1,115 @@
+package Group::Git::Cmd::Todo;
+
+# Created on: 2013-05-10 07:05:17
+# Create by:  Ivan Wills
+# $Id$
+# $Revision$, $HeadURL$, $Date$
+# $Revision$, $Source$, $Date$
+
+use strict;
+use version;
+use Moose::Role;
+use Carp;
+use Data::Dumper qw/Dumper/;
+use English qw/ -no_match_vars /;
+use File::chdir;
+use Path::Tiny;
+
+our $VERSION = version->new('0.0.2');
+
+my $opt = Getopt::Alt->new(
+    {
+        helper  => 1,
+        help    => __PACKAGE__,
+        default => {
+            min => 1,
+        },
+    },
+    [
+        'min|min-commits|m=i',
+        'name|n',
+        'no_release|no-release',
+        'verbose|v+',
+        'quiet|q!',
+    ]
+);
+
+my $todo;
+sub todo {
+    my ($self, $name) = @_;
+
+    return unless -d $name;
+
+    $opt->process if !%{ $opt->opt || {} };
+
+    local $CWD = $name;
+
+    return if !-f 'TODO.md';
+
+    $todo .= "$name\n\n" . path('TODO.md')->slurp;
+}
+
+sub todo_end {
+    print $todo;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Group::Git::Cmd::Todo - Group-Git extra tools
+
+=head1 VERSION
+
+This documentation refers to Group::Git::Cmd::Todo version 0.0.2
+
+
+=head1 SYNOPSIS
+
+   use Group::Git::Cmd::Todo;
+
+   # Brief but working code example(s) here showing the most common usage(s)
+   # This section will be as far as many users bother reading, so make it as
+   # educational and exemplary as possible.
+
+
+=head1 DESCRIPTION
+
+Just the holder module
+
+=head1 SUBROUTINES/METHODS
+
+=head1 DIAGNOSTICS
+
+=head1 CONFIGURATION AND ENVIRONMENT
+
+=head1 DEPENDENCIES
+
+=head1 INCOMPATIBILITIES
+
+=head1 BUGS AND LIMITATIONS
+
+There are no known bugs in this module.
+
+Please report problems to Ivan Wills (ivan.wills@gmail.com).
+
+Patches are welcome.
+
+=head1 AUTHOR
+
+Ivan Wills - (ivan.wills@gmail.com)
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright (c) 2013 Ivan Wills (14 Mullion Close, Hornsby Heights, NSW Australia 2077).
+All rights reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself. See L<perlartistic>.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.
+
+=cut
